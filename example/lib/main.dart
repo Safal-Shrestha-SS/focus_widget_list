807ca4838d6cfd7461e36cd5b0f5594c6973ae65
unknown <safalshrestha721156@gmail.com>
2022-12-23 11:09:01 +0545

    Changes made  - Added functionality to pass tap through  - Added new example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 import 'package:focus_widget_list/focus_widget_list.dart';
 
 void main() {
+  debugRepaintRainbowEnabled = true;
   runApp(const MyApp());
 }
 
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -33,13 +37,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void disable() {
+  void _incrementCounter() {
     setState(() {
-      disbleFocus = false;
+      test = false;
     });
   }
 
-  bool disbleFocus = false;
+  int index = 0;
+  bool test = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,21 +52,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          fit: StackFit.passthrough,
           children: <Widget>[
-            Expanded(
-                child: FocusWidget(
-              widgetList: listOfWidget,
-              blur: disbleFocus,
-              blurColor: Colors.blue,
-              blurValue: 0.7,
-            ))
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              child: FocusWidget(
+                widgetList: listOfWidget,
+                blur: false,
+                index: index,
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: disable,
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
@@ -69,49 +76,85 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 List<Widget> listOfWidget = [
-  Container(
-    height: 50,
-    color: Colors.amber[600],
-    child: const Center(child: Text('Entry A')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        height: 50,
+        color: Colors.amber[600],
+        child: const Center(child: Text('Entry A')),
+      ),
+    ),
   ),
-  Container(
-    height: 50,
-    color: Colors.amber[500],
-    child: const Center(child: Text('Entry B')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: Container(
+      height: 50,
+      color: Colors.amber[500],
+      child: const Center(child: Text('Entry B')),
+    ),
   ),
-  Container(
-    height: 50,
-    color: Colors.amber[100],
-    child: const Center(child: Text('Entry C')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: Container(
+      height: 50,
+      color: Colors.amber[100],
+      child: const Center(child: Text('Entry C')),
+    ),
   ),
-  Container(
-    height: 50,
-    color: Colors.amber[600],
-    child: const Center(child: Text('Entry A')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: Container(
+      height: 50,
+      color: Colors.amber[600],
+      child: const Center(child: Text('Entry A')),
+    ),
   ),
-  Container(
-    height: 50,
-    color: Colors.amber[500],
-    child: const Center(child: Text('Entry B')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: Container(
+      height: 50,
+      color: Colors.amber[500],
+      child: const Center(child: Text('Entry B')),
+    ),
   ),
-  Container(
-    height: 50,
-    color: Colors.amber[100],
-    child: const Center(child: Text('Entry C')),
-  ),
-  Container(
-    height: 50,
-    color: Colors.amber[600],
-    child: const Center(child: Text('Entry A')),
-  ),
-  Container(
-    height: 50,
-    color: Colors.amber[500],
-    child: const Center(child: Text('Entry B')),
-  ),
-  Container(
-    height: 50,
-    color: Colors.amber[100],
-    child: const Center(child: Text('Entry C')),
+  CustomPopupMenu(
+    pressType: PressType.singleClick,
+    barrierColor: Colors.transparent,
+    enablePassEvent: true,
+    menuBuilder: () {
+      return Container(color: Colors.blue, child: const Text('data'));
+    },
+    child: Container(
+      height: 50,
+      color: Colors.amber[100],
+      child: const Center(child: Text('Entry C')),
+    ),
   ),
 ];
--- lib/src/focus.dart
+++ lib/src/focus.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 import 'notifiers.dart';
@@ -7,24 +8,29 @@ class FocusWidget extends StatelessWidget {
   final bool blur;
   final Color blurColor;
   final double blurValue;
-  const FocusWidget({
-    Key? key,
-    required this.widgetList,
-    required this.blur,
-    this.blurColor = Colors.black,
-    this.blurValue = 0.5,
-  }) : super(key: key);
+  final bool allBlur;
+  final int index;
+  const FocusWidget(
+      {Key? key,
+      required this.widgetList,
+      required this.blur,
+      this.blurColor = Colors.black,
+      this.blurValue = 0.5,
+      this.allBlur = true,
+      this.index = 0})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ValueNotifier<bool> focus = ValueNotifier(blur);
-    final ValueNotifier<int> focusIndex = ValueNotifier(0);
+    final ValueNotifier<int> focusIndex = ValueNotifier(index);
 
     return ListView.builder(
         itemCount: widgetList.length,
         itemBuilder: (BuildContext context, index) {
-          return GestureDetector(
-            onTap: () {
+          return Listener(
+            behavior: HitTestBehavior.translucent,
+            onPointerDown: (event) {
               focus.value = true;
               focusIndex.value = index;
             },
@@ -35,7 +41,8 @@ class FocusWidget extends StatelessWidget {
                   return Stack(
                     children: [
                       widgetList[index],
-                      IgnorePointer(
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
                         child: Container(
                           color: focusValue && indexValue != index
                               ? blurColor.withOpacity(blurValue)
